34e732b0a64762d50edb6efe27dd9b7bc09ad0a3
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2022-01-03 13:05:32 +0600

    [Add] - banner section

--- src/Components/Section/Banner/Banner.js
+++ src/Components/Section/Banner/Banner.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Banner.css';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import silder1 from './../../../images/slider-1.jpg';
+import silder2 from './../../../images/slider-2.jpg';
+import silder3 from './../../../images/slider-3.jpg';
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+        <Carousel>
+            <div>
+                <img src={silder1} alt='1' />
+            </div>
+            <div>
+                <img src={silder2} alt='2' />
+            </div>
+            <div>
+                <img src={silder3} alt='3' />
+            </div>
+        </Carousel>
     );
 };
 
