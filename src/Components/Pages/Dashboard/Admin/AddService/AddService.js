b514bbfc1779dcac2d787b56fe5a9a19f81f0ade
cseswapon <cseswaponsaha@gmail.com>
2022-01-04 02:34:54 +0600

    [MOD] - all nasted route modify

---
+++ src/Components/Pages/Dashboard/Admin/AddService/AddService.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+const AddService = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) =>{ console.log(data)
+reset()}
+  return (
+    <div className="containter">
+      <h1>Add Product</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="Title" {...register("title", { required: true })} />
+        <textarea
+          placeholder="Discretion"
+          {...register("Discretion", { required: true })}
+        />
+        <input
+          placeholder="Image Url"
+          {...register("url", { required: false })}
+        />
+        <input
+          placeholder="Per Person Service"
+          type="number"
+          {...register("person", { required: true })}
+        />
+        {errors.exampleRequired && (
+          <span className="text-danger">This field is required</span>
+        )}
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default AddService;
---
+++ src/Components/Pages/Dashboard/Admin/ManageService/ManageService.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageService = () => {
+    return (
+        <div>
+            <h1>This is mage service</h1>
+        </div>
+    );
+};
+
+export default ManageService;
