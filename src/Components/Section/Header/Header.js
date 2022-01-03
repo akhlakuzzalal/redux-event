22def134736a4bcbe893f5aa97f3c2f6dca49eb2
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2022-01-03 12:23:13 +0600

    [Add] - header design and router

--- src/Components/Section/Header/Header.js
+++ src/Components/Section/Header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand as = {Link} to="/">Redux Event</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav>
+                        <Nav.Link as = {Link} to="/">Home</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link as = {Link} to="/services">Services</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link as = {Link} to="/login">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
