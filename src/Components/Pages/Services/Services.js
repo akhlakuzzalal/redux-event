b8e28feeca526564977eeb55ddd1ddee9fd9c004
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2022-01-03 23:58:51 +0600

    [Mod] - design update

---
+++ src/Components/Pages/Services/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Service from '../../Section/Service/Service';
+
+const Services = () => {
+    return (
+        <div>
+            <Service></Service>
+        </div>
+    );
+};
+
+export default Services;
