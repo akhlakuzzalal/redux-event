3afff55c8c030c017eac20edae84ea1302feb737
cseswapon <cseswaponsaha@gmail.com>
2022-01-04 23:38:12 +0600

    [ADD] - order list

--- src/reducer/index.reducer.js
+++ src/reducer/index.reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import users from "./users";
 import services from "./servicesState";
 import order from "./orderState";
+import allOrder from "./allOrders";
 const allReducer = combineReducers({
   users,
   services,
   order,
+  allOrder,
 });
 
 export default allReducer;
