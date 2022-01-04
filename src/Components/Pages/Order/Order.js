689e97fe91b74427404d9050c9898c94b5f530b4
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-01-04 15:11:30 +0600

    [ADD] - order

--- src/Components/Pages/Order/Order.js
+++ src/Components/Pages/Order/Order.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 
 const Order = () => {
-   const { id } = useParams()
+   const [service, setService] = useState([]);
+   const [amount, setAmount] = useState(0);
+   const { id } = useParams();
+   useEffect(() => {
+      fetch(`http://localhost:5000/service/${id}`)
+         .then(res => res.json())
+         .then(data => setService(data));
+   }, [id])
+   const { register, handleSubmit } = useForm();
+   const amountCalc = (person) => {
+      setAmount(person * service.amount)
+      console.log(person)
+   }
+   const onSubmit = data => {
+      const finallData = { ...data, payment: amount };
+      console.log(finallData)
+   };
+   console.log(amount.toString())
    return (
       <div>
          <h2>Order for {id}</h2>
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <input  {...register("name")} placeholder='Name' />
+            <input {...register("email")} placeholder='Email' />
+            <input {...register("phone")} placeholder='Phone Number' />
+            <input {...register("date")} placeholder='Event Date' />
+            <input onKeyUp={(e) => amountCalc(e.target.value)} {...register("person")} placeholder='Totall person' />
+            <p>{amount}</p>
+            <input type="submit" value={'Pay Now'} />
+         </form>
       </div>
    );
 };
