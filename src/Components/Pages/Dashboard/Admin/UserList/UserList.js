6ef81095788369779ff8e672ad5bb2d0fa93ef76
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-01-04 12:57:45 +0600

    [SLV]-redux-issue

--- src/Components/Pages/Dashboard/Admin/UserList/UserList.js
+++ src/Components/Pages/Dashboard/Admin/UserList/UserList.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const UserList = () => {
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
     return (
         <div>
             <h1>user list</h1>
